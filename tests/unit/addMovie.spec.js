141124ba2f88fbe81311c29444efb37860d9137b
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2019-10-12 15:11:17 +0530

    one more unit test

--- tests/unit/addMovie.spec.js
+++ tests/unit/addMovie.spec.js
@@ -17,4 +17,15 @@ describe("AddMovie.vue", () => {
     });
     expect(wrapper.element).toMatchSnapshot();
   });
+
+  it("Check initial state", () => {
+    const addNewMovie = jest.fn();
+    const wrapper = shallowMount(AddMovie, {
+      propsData: { addNewMovie }
+    });
+    expect(wrapper.vm.movieName).toBe(null);
+    expect(wrapper.vm.genre).toBe(null);
+    expect(wrapper.vm.rating).toBe(null);
+    expect(wrapper.vm.website).toBe(null);
+  });
 });
