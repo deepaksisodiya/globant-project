67b8daf10a88999c6369ad526eb1378089cf29e8
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2019-10-12 13:50:55 +0530

    list movie component

---
+++ src/components/ListMovie.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <ul v-for="(movie, index) in movies" :key="index">
+      {{
+        movie.movieName
+      }}
+    </ul>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "ListMovie",
+  props: {
+    movies: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
