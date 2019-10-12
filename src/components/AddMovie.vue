2bc8446b4eec4a1611a462be3563643b00c8c897
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2019-10-12 13:37:27 +0530

    added form

---
+++ src/components/AddMovie.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Add Movie to list</h1>
+    <form>
+      <p>
+        <label for="movieName">Movie Name* </label>
+        <input v-model="movieName" type="text" />
+      </p>
+      <p>
+        <label for="genre">Genre*</label>
+        <select v-model="genre">
+          <option>Star Wars</option>
+          <option>Vanilla Sky</option>
+          <option>Atomic Blonde</option>
+        </select>
+      </p>
+      <p>
+        <label for="rating">Rating* </label>
+        <input v-model="rating" type="number" min="0" max="5" />
+      </p>
+      <p>
+        <label for="website">Website </label>
+        <input v-model="website" type="text" />
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddMovie",
+  data() {
+    return {
+      movieName: "",
+      genre: "",
+      rating: null,
+      website: ""
+    };
+  }
+};
+</script>
+
+<style></style>
